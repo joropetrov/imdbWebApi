5ada37f052b82345ea5adcb8ed470386a45b73e4
GKP <red_dragon_1@abv.bg>
2021-12-01 14:20:29 +0200

    post "/" add

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 var express = require("express");
 var request = require("request");
 var app = express();
-// Need to tell ejs about the library volder
 app.use(express.static("lib"));
- 
-     
 app.set("view engine", "ejs");
  
 var bodyParser = require("body-parser");
 const { response } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
- 
+app.use(express.urlencoded({extended: false}));
 app.listen(3000, function() { 
   console.log('Server listening on port 3000'); 
 });
- 
+
+let imdbiSearchMovieByNamePath = "https://www.omdbapi.com/?s=inception&apikey=ab666ed2";
+
 app.get("/", function(request, response){
     response.render("page");
 });
  //tt3896198&apikey=ab666ed2
-app.get("/results", function(req, res){ 
-  //http://www.omdbapi.com/?s=inception&apikey=[yourkey]
-  //https://www.omdbapi.com/?t=33&apikey=ab666ed2
-  request("https://www.omdbapi.com/?s=inception&apikey=ab666ed2", 
+app.get("/resultsByName", function(req, res){ 
+  request(imdbiSearchMovieByNamePath, 
       function(error, response, body){
           if(!error && response.statusCode == 200){
+            
               var data = JSON.parse(body);
-              console.log(data['Search'][5]['Poster'])
+              res.render("resultsByName", {data: data});
+              
+          }
+  });
+});
+
+app.post("/", (req, res) =>{
+  const {name} = req.body;
+  const {year} = req.body;
+     if (name) {
+      return res.status(200).send(`welcome ${name}`);
+     } else if (year) {
+      return res.status(200).send(`years ${year}`);
+     }
+    
+  
+})
+
+app.get("/results", function(req, res){ 
+  request(imdbiSearchMovieByNamePath, 
+      function(error, response, body){
+          if(!error && response.statusCode == 200){
+           
+              var data = JSON.parse(body);
               res.render("results", {data: data});
           }
   });
 });
-// app.get("/results", function(request, response){ 
-//   response.render("results");
-// });
  
 app.get("*", function(request, response){
   response.render("error");
